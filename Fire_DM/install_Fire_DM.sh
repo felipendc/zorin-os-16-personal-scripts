93fb35a445c1343b8d74e538d817b982bda061b6
felipendc <felipendc10@gmail.com>
2021-08-21 00:48:52 -0300

    FireDM refactored the installation script and fixed icon

--- Fire_DM/install_Fire_DM.sh
+++ Fire_DM/install_Fire_DM.sh
@@ -27,33 +27,6 @@ sudo apt install -y \
 # Installing FireDM
 python3 -m pip install firedm --user --upgrade --no-cache
 
-
-# Removing the temp folder if it exist
-if [ -d "temp_files" ]; then 
-    rm -R temp_files
-fi
-
-# Creating and accessing the tempo_files folder
-mkdir temp_files
-cd temp_files
-
-# Creating the FireDM deskto file and appending its specs
-cat >> firedm.desktop <<EOF
-echo "[Desktop Entry]"
-Type=Application
-Version=1.0
-Name=FireDM
-Comment=Python open source alternative to IDM
-Path=/usr/bin
-Exec=firedm
-Icon=firedm
-Terminal=false
-Categories=Utility;AudioVideo;Network;
-Keywords=Internet;download
-EOF
-
-
-# Copying the .desktop to its proper folder
 sudo cp -R $(pwd)/firedm.desktop /usr/share/applications/firedm.desktop
 
 # Copying the icon to its proper folder
@@ -62,23 +35,17 @@ if [ ! -d "$icon_folder" ]; then
     mkdir -p $icon_folder
 fi
 
-# cp -R ../icons/firedm.png ~/.local/share/icons/hicolor/48x48/apps/firedm.png
-# sudo cp -R ../icons/firedm.png /usr/share/icons/hicolor/48x48/apps/firedm.png
+# cp -R icons/firedm.png ~/.local/share/icons/hicolor/48x48/apps/firedm.png
+# sudo cp -R icons/firedm.png /usr/share/icons/hicolor/48x48/apps/firedm.png
 
-cp -R ../icons/firedm.png ~/.local/share/icons/hicolor/48x48/apps/firedm.png
-sudo cp -R ../icons/firedm.png /usr/share/pixmaps/iredm.png
+cp -R icons/firedm.png ~/.local/share/icons/hicolor/48x48/apps/firedm.png
+sudo cp -R icons/firedm.png /usr/share/pixmaps/firedm.png
 
 # Updating the icons cache:
 sudo gtk-update-icon-cache /usr/share/icons/hicolor/
 sudo gtk-update-icon-cache
+xdg-desktop-menu forceupdate --mode system
 
-# Going back to the parent folder
-cd ../
-
-# Removing the temp folder if it exist
-if [ -d "temp_files" ]; then 
-    rm -R temp_files
-fi
 
 # TO WRAP IT UP, YOU WILL NEED TO REBOOT THE COMPUTER, SO THAT, 
 # THE ICON WILL SHOW UP ON THE APLICATIONS MENU.
--- TEMPORARIO/aaa/install_Fire_DM.sh
+++ TEMPORARIO/aaa/install_Fire_DM.sh
@@ -27,7 +27,34 @@ sudo apt install -y \
 # Installing FireDM
 python3 -m pip install firedm --user --upgrade --no-cache
 
-xdg-desktop-menu install --mode system $(pwd)'/firedm.desktop' && xdg-desktop-menu forceupdate --mode system
+
+# Removing the temp folder if it exist
+if [ -d "temp_files" ]; then 
+    rm -R temp_files
+fi
+
+# Creating and accessing the tempo_files folder
+mkdir temp_files
+cd temp_files
+
+# Creating the FireDM deskto file and appending its specs
+cat >> firedm.desktop <<EOF
+echo "[Desktop Entry]"
+Type=Application
+Version=1.0
+Name=FireDM
+Comment=Python open source alternative to IDM
+Path=/usr/bin
+Exec=firedm
+Icon=firedm
+Terminal=false
+Categories=Utility;AudioVideo;Network;
+Keywords=Internet;download
+EOF
+
+
+# Copying the .desktop to its proper folder
+sudo cp -R $(pwd)/firedm.desktop /usr/share/applications/firedm.desktop
 
 # Copying the icon to its proper folder
 icon_folder="~/.local/share/icons/hicolor/48x48/apps"
@@ -39,12 +66,19 @@ fi
 # sudo cp -R ../icons/firedm.png /usr/share/icons/hicolor/48x48/apps/firedm.png
 
 cp -R ../icons/firedm.png ~/.local/share/icons/hicolor/48x48/apps/firedm.png
-sudo cp -R ../icons/firedm.png /usr/share/pixmaps/firedm.png
+sudo cp -R ../icons/firedm.png /usr/share/pixmaps/iredm.png
 
 # Updating the icons cache:
 sudo gtk-update-icon-cache /usr/share/icons/hicolor/
 sudo gtk-update-icon-cache
 
+# Going back to the parent folder
+cd ../
+
+# Removing the temp folder if it exist
+if [ -d "temp_files" ]; then 
+    rm -R temp_files
+fi
 
 # TO WRAP IT UP, YOU WILL NEED TO REBOOT THE COMPUTER, SO THAT, 
 # THE ICON WILL SHOW UP ON THE APLICATIONS MENU.
